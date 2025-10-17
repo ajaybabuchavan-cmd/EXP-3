166c9aa2c2ba40c449788b67b035dedd4fae9d4e
ajaybabuchavan-cmd <ajaybabuchavan@eng.rizvi.edu.in>
2025-10-17 13:49:58 +0530

    Add files via upload

---
+++ E-3 TASK-1 OUTPUT.c
@@ -0,0 +1,23 @@
+
+*** Calculator Menu ***
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+5. Exit
+Enter your choice: 3
+Enter two numbers: 4
+4
+Result = 16.00
+
+*** Calculator Menu ***
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+5. Exit
+Enter your choice: 5
+Exiting...
+
+
+=== Code Execution Successful ===
